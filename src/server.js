f1c0d6c230bf869c7a51b3992c0d40f3f422d7f0
soranoh <sora2019@naver.com>
2021-11-30 15:10:04 +0900

    #1.4 WebSocket Messages

--- src/server.js
+++ src/server.js
@@ -16,7 +16,14 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({server});
 
 wss.on("connection", (socket) => {
-    console.log(socket);
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser");
+    })
+    socket.on("message", (message) => {
+        console.log(message.toString());
+    })
+    socket.send("hello");
 });
 
 server.listen(3000, handleListen);
