47eae74bad348290cc88e9a4b8180c743f1ddccb
soranoh <sora2019@naver.com>
2021-11-30 16:09:41 +0900

    #1.6 Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,28 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    console.log(message.data);
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from the Server");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 1000);
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const input = messageForm.querySelector("input");
+    const inputValue = input.value;
+    socket.send(inputValue);
+
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
