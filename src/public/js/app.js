a933469acac8c7c844a73b1fecccfa42550ca282
soranoh <sora2019@naver.com>
2021-12-02 16:12:41 +0900

    #3.1 Call Controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,27 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+
+            camerasSelect.appendChild(option);
+        });
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 async function getMedia() {
     try {
@@ -15,12 +32,18 @@ async function getMedia() {
             video: true
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch(error) {
         console.log(error);
     }
 }
 
 function handleMuteClick() {
+    myStream
+        .getAudioTracks()
+        .forEach((track) => (
+        track.enabled = !track.enabled));
+
     if(!muted) {
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -31,6 +54,10 @@ function handleMuteClick() {
 }
 
 function handleCameraClick() {
+    myStream
+        .getVideoTracks()
+        .forEach((track) => (
+            track.enabled = !track.enabled));
     if(cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
