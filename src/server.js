23952931158ed53f9fff50e0c3046df17a4abf0c
soranoh <sora2019@naver.com>
2021-12-03 10:50:34 +0900

    #3.3 Introduction to WebRTC

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
 
+/*
+ * [WebRTC]
+ * - peer to peer 연결
+ * - 서버를 지나가지 않고 비디오, 텍스트, 오디오 등 데이터를 직접 전달
+
+ * 1) 브라우저가 서버에 본인의 configuration, setting, location 등 정보를 먼저 전달.
+ * 2) 서버는 받은 정보를 다른 브라우저에 전달.
+ * 3) 위의 signalling 이 끝나면 peer to peer 연결 가능.
+ */
+
 const app = express();
 
 app.set("view engine", "pug");
